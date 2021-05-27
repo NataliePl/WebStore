ef6ea636ba00d98f562a82f70b13f0cd7f22ef47
Natalie_Pl <n.plaksickaya@yandex.ru>
2021-05-27 15:42:35 +0300

    Сотрудники магазина

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,11 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
+
         private readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration) { _Configuration = Configuration; }
@@ -21,6 +30,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content(_Configuration["Greetings"]);
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
